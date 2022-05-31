d38d99fa90febf4e80b76ba62dbf1bb4a9a4e3f9
lidesia <lidesia@archDesktop>
2022-05-31 14:37:38 -0400

    Updated GUI to include license and a link to the githubRepo

--- nameParser/src/gui/NameParserGUI.java
+++ nameParser/src/gui/NameParserGUI.java
@@ -25,7 +25,7 @@ public class NameParserGUI extends JFrame implements ActionListener {
 	/**
 	 * Defines the number of rows in the panel
 	 */
-	private final int NUM_ROWS = 1;
+	private final int NUM_ROWS = 2;
 	
 	/**
 	 * Defines the number of columns in the panel
@@ -46,6 +46,13 @@ public class NameParserGUI extends JFrame implements ActionListener {
 	 * Quit button for the program
 	 */
 	private static JButton quit;
+	
+	/**
+	 * Button that displays the License
+	 */
+	private static JButton license;
+	
+	
 	
 	/**
 	 * Frame where everything is placed
@@ -86,17 +93,20 @@ public class NameParserGUI extends JFrame implements ActionListener {
 		start = new JButton("Start");
 		help = new JButton("Help");
 		quit = new JButton("Quit");
+		license = new JButton("License");
 		
 		
 		//add an action listener to the buttons so we can know when they are pressed
 		start.addActionListener(this);
 		help.addActionListener(this);
 		quit.addActionListener(this);
+		license.addActionListener(this);
 		
 		// Add our newly created buttons to the holder and then to the display
 		panelHolder[0][0].add(start);
 		panelHolder[0][1].add(help);
 		panelHolder[0][2].add(quit);
+		panelHolder[1][1].add(license);
 	}
 
 	public static void main(String[] args) {
@@ -110,6 +120,9 @@ public class NameParserGUI extends JFrame implements ActionListener {
 		} else if (e.getSource() == help) {
 			JOptionPane.showMessageDialog(frame, "To use this program, you must first have \n"
 					+ "1) A master student file\n2) Some of \"filter\" files");
+		} else if (e.getSource() == license) {
+			JOptionPane.showMessageDialog(frame, "This program is licensed under GPLV3 or later.\nFor more information"
+					+ "visit https://github.com/jprhyne/nameParser.");
 		} else if (e.getSource() == start) {
 			JFileChooser fileChooser = new JFileChooser();
 			FileNameExtensionFilter filter = new FileNameExtensionFilter("CSV files", "csv");
